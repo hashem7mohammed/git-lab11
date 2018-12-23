aa0016df293b01488408aa8bc736fdb71eabed40
= <hashemhejazy7@gmail.com>
2018-12-23 11:41:15 +0200

    six commit

--- src/lab11/Lab11.java
+++ src/lab11/Lab11.java
@@ -7,7 +7,8 @@ public class Lab11 {
   
     public static void main(String[] args) {
         System.out.println("a java");
-        
+                System.out.println("welcome");
+
         
         
     }
